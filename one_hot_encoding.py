1b9eefd8fac55e55e91ac985dfb7d326212dc7a3
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-04-22 00:28:58 -0400

    Add files via upload

---
+++ one_hot_encoding.py
@@ -0,0 +1,20 @@
+import numpy as np
+### Categorical variable with 4 categories to be converted to numeric data
+exercises_bases = ["A", "G", "C", "U"]
+
+### Full list of RNA nucleotide bases
+full_bases = ["G", "U", "C", "A"]
+
+### map each base to an integer
+mapping = {}
+for x in range(len(full_bases)):
+  mapping[full_bases[x]] = x
+
+one_hot_encode = []
+
+for c in exercises_bases:
+  arr = list(np.zeros(len(full_bases), dtype = int))
+  arr[mapping[c]] = 1
+  one_hot_encode.append(arr)
+
+print(one_hot_encode)
